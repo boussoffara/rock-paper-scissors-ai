df1ee4ca39fe0afb4999c3f8d374537b44eb1385
boussoffara <boussoffara@me.com>
2020-07-22 18:24:37 +0200

    clean code

--- app/app.py
+++ app/app.py
@@ -1,43 +1,66 @@
-
 from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask import send_from_directory
-from ai import *
+import random
+from ai import player, history, memory, level, ensemble_min_score, models_inp, models_out, models_ens, models
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test-db.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test-db.sqlite3"
 db = SQLAlchemy(app)
+output = ""
 
 
-
-
-
-@app.route('/')
+@app.route("/")
 def hello():
-  return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/play',methods=['POST'])
+
+@app.route("/play", methods=["POST"])
 def play():
-    global  output
-    if output=="":
-        first=random.choice("RPS")
-        output=player(request.form['me'].upper(),first,history,memory,level,ensemble_min_score,models_inp,models_out,models_ens,models)
-        return jsonify({'him': first.lower()})
+    global output
+    if output == "":
+        first = random.choice("RPS")
+        output = player(
+            request.form["me"].upper(),
+            first,
+            history,
+            memory,
+            level,
+            ensemble_min_score,
+            models_inp,
+            models_out,
+            models_ens,
+            models,
+        )
+        return jsonify({"him": first.lower()})
     else:
-        ret=output
-        output=player(request.form['me'].upper(),output,history,memory,level,ensemble_min_score,models_inp,models_out,models_ens,models)
-        return jsonify({'him': ret.lower()})
-
+        ret = output
+        output = player(
+            request.form["me"].upper(),
+            output,
+            history,
+            memory,
+            level,
+            ensemble_min_score,
+            models_inp,
+            models_out,
+            models_ens,
+            models,
+        )
+        return jsonify({"him": ret.lower()})
 
     print(history.history)
-    return jsonify({'him': output.lower()})
+    return jsonify({"him": output.lower()})
 
-@app.route('/favicon.ico')
+
+@app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(
+        os.path.join(app.root_path, "static"), "favicon.ico", mimetype="image/vnd.microsoft.icon"
+    )
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)

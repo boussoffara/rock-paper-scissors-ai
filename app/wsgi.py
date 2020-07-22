219d4e6d630719bb00936b30c03ee18ae30ef4f7
boussoffara <boussoffara@me.com>
2020-07-22 18:12:27 +0200

    test

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,11 @@
-from app import *
+from flask_sqlalchemy import SQLAlchemy
+from ai import *
+from app import app
+
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test-db.sqlite3"
+db = SQLAlchemy(app)
 
 if __name__ == "main":
     app.run()
